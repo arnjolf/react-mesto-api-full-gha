a144a7943dfdfb3b692e93721f89d8765111cf71
arnjolf <arnjolff@gmail.com>
2023-08-04 17:28:57 +0600

    jwt auth secret-key fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -10,7 +12,11 @@ module.exports = (req, res, next) => {
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(
+      token,
+      // eslint-disable-next-line comma-dangle
+      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'
+    );
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
